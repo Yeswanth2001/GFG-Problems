e1d00f15c1be2c3a7914b8a1d85aa0832e9cc45a
Yeswanth S <yeswanth1632001@gmail.com>
2024-05-03 10:42:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest consecutive subsequence/longest-consecutive-subsequence.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.close();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   if(arr.length < 1) return 0;
+        Arrays.sort(arr);
+
+        int res = 1, max = 1;
+        for(int i=0;i<arr.length-1;i++) {
+            if(arr[i+1]-arr[i] == 1) {
+                res++;
+                max = Math.max(max,res);
+            } 
+            else if(arr[i+1] != arr[i]){
+                res = 1;
+            }
+        }
+        return max;
+	}
+}
